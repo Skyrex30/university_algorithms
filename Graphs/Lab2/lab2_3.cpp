8c2399c94f4123ed5db9b996629be7d3aa655c6b
Nagy Csongor <nagycsongor2004@protonmail.com>
2024-12-12 20:14:50 +0200

    Added lab2

---
+++ Graphs/Lab2/lab2_3.cpp
@@ -0,0 +1,80 @@
+﻿/*
+
+Adott egy összefüggő irányított egyszerű gráf. Állapítsuk meg, hogy a gráf tartalmaz-e kört!
+
+Bemeneti adatok
+
+A graf.in állomány első sorában n és m értéke (1 <= n <= 1000, 1 <= m <= 499500). A következő sorokban a csomópontpárok találhatóak szóközzel elválasztva.
+
+Kimeneti adatok
+
+Egyetlen szó (igen vagy nem).
+*/
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <unordered_set>
+
+using namespace std;
+
+vector<vector<int>> graph;
+
+bool hasCycleUtil(int node, vector<bool>& visited, vector<bool>& recStack) {
+    if (!visited[node]) {
+        visited[node] = true;
+        recStack[node] = true;
+
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor] && hasCycleUtil(neighbor, visited, recStack)) {
+                return true;
+            }
+            else if (recStack[neighbor]) {
+                return true;
+            }
+        }
+    }
+    recStack[node] = false;
+    return false;
+}
+
+bool hasCycle(int n) {
+    vector<bool> visited(n + 1, false);
+    vector<bool> recStack(n + 1, false);
+
+    for (int i = 1; i <= n; ++i) {
+        if (hasCycleUtil(i, visited, recStack)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ifstream infile("graf.in");
+    int n, m;
+    infile >> n >> m;
+
+    graph.resize(n + 1);
+
+
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        infile >> u >> v;
+        graph[u].push_back(v);
+    }
+    infile.close();
+
+    bool hasCycleResult = hasCycle(n);
+
+    ofstream outfile("graf.out");
+    if (hasCycleResult) {
+        outfile << "igen" << endl;
+    }
+    else {
+        outfile << "nem" << endl;
+    }
+    outfile.close();
+
+    return 0;
+}
---
+++ Graphs/Lab2/lab2_4.cpp
@@ -0,0 +1,63 @@
+﻿/*
+
+Rendezzük egy körmentes egyszerű irányított gráf csúcsait olyan sorrendbe, hogy minden él balról jobbra (egy korábbi csúcsból egy későbbi csúcsba) mutasson.
+
+Bemeneti adatok
+
+A graf.in állomány első sorában n és m értéke (1 <= n <= 1000, 1 <= m <= 499500). A következő sorokban a csomópontpárok találhatóak szóközzel elválasztva.
+
+Kimeneti adatok
+
+Szóközzel elválasztva a csomópontok sorszáma.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<vector<int>> adjList; 
+vector<bool> visited;
+stack<int> sorted;
+
+void dfs(int v) {
+    visited[v] = true;
+    for (int u : adjList[v]) {
+        if (!visited[u]) {
+            dfs(u);
+        }
+    }
+    sorted.push(v);
+}
+
+int main() {
+    int n, m, u, v;
+    ifstream input("graf.in");
+
+    input >> n >> m;
+
+    adjList.resize(n);
+    visited.assign(n, false);
+
+    for (int i = 0; i < m; i++) {
+        input >> u >> v;
+
+        adjList[u - 1].push_back(v - 1);
+    }
+
+
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            dfs(i);
+        }
+    }
+
+    while (!sorted.empty()) {
+        cout << sorted.top() + 1 << " ";
+        sorted.pop();
+    }
+
+    return 0;
+}
